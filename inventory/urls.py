432c7e1b38728801b53bb0a865b007853305f4f4
Dan Meliza <dan@meliza.org>
2016-11-02 17:57:24 -0400

    fixes for django 1.10

--- inventory/urls.py
+++ inventory/urls.py
@@ -1,10 +1,10 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
 from inventory import views
 
-urlpatterns = patterns('',
-                       url(r'^$', views.index, name='index'),
-                       url(r'^orders/$', views.OrdersView.as_view(), name='orders'),
-                       url(r'^orders/(?P<pk>\d+)/$', views.OrderView.as_view(), name='order'),
-                       url(r'^items/(?P<pk>\d+)/$', views.ItemView.as_view(), name='item')
-)
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^orders/$', views.OrdersView.as_view(), name='orders'),
+    url(r'^orders/(?P<pk>\d+)/$', views.OrderView.as_view(), name='order'),
+    url(r'^items/(?P<pk>\d+)/$', views.ItemView.as_view(), name='item')
+]
