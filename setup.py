a0c4c9f03c74f29b12eb5e727064652b6e216080
Dan Meliza <dan@meliza.org>
2017-06-23 17:18:21 -0400

    add migration

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except ImportError:
     use_setuptools()
     from setuptools import setup
 
-VERSION = '0.3.2'
+VERSION = '0.3.3'
 cls_txt = """
 Development Status :: 3 - Alpha
 Framework :: Django
