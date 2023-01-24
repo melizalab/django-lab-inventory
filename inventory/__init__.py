6d804a7d6fbf17cf787dcd85c24d69fe1f2e3587
melizalab <melizalab@pholia.lan>
2023-01-24 14:59:29 -0500

    tick version; setup.py -> setup.cfg

---
+++ inventory/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+# -*- mode: python -*-
+__version__ = "0.4.2"
+
--- setup.py
+++ setup.py
@@ -1,33 +1,4 @@
 # -*- coding: utf-8 -*-
 # -*- mode: python -*-
-import os
-import sys
-if sys.hexversion < 0x03060000:
-    raise RuntimeError("Python 3.6 or higher required")
 from setuptools import setup
-
-VERSION = '0.4.1'
-cls_txt = """
-Development Status :: 3 - Alpha
-Framework :: Django
-Intended Audience :: Science/Research
-License :: OSI Approved :: GNU General Public License (GPL)
-Programming Language :: Python
-Topic :: Scientific/Engineering
-Topic :: Internet :: WWW/HTTP
-Topic :: Internet :: WWW/HTTP :: Dynamic Content
-"""
-
-setup(
-    name="django-lab-inventory",
-    version=VERSION,
-    description="A simple Django app for managing lab inventory",
-    long_description=open(os.path.join(os.path.dirname(__file__), 'README.md')).read(),
-    long_description_content_type="text/markdown",
-    classifiers=[x for x in cls_txt.split("\n") if x],
-    author='C Daniel Meliza',
-    maintainer='C Daniel Meliza',
-    url="https://github.com/melizalab/django-lab-inventory",
-    packages=['inventory'],
-    include_package_data=True,
-)
+setup()
