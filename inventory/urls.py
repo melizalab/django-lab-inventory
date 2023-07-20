9f1806fff92d9ae041627aba914e582c68d32790
Dan Meliza <dan@meliza.org>
2023-07-20 15:09:41 -0400

    add item list and search for orders and items (closes #7)

--- inventory/urls.py
+++ inventory/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     re_path(r"^$", views.index, name="index"),
     re_path(r"^orders/$", views.OrderList.as_view(), name="orders"),
     re_path(r"^orders/(?P<pk>\d+)/$", views.OrderView.as_view(), name="order"),
+    re_path(r"^items/$", views.ItemList.as_view(), name="items"),
     re_path(r"^items/(?P<pk>\d+)/$", views.ItemView.as_view(), name="item"),
 ]
--- inventory/views.py
+++ inventory/views.py
@@ -7,6 +7,7 @@ from django.urls import reverse
 from django.views import generic
 import django_filters as filters
 from django_filters.views import FilterView
+from django.db.models import Q
 
 from inventory.models import Order, Item, Account
 
@@ -15,37 +16,7 @@ def index(request):
     return render(request, "inventory/index.html")
 
 
-class OrderFilter(filters.FilterSet):
-    name = filters.CharFilter(field_name="name", lookup_expr="icontains")
-    ordered_by = filters.CharFilter(
-        field_name="ordered_by__username", lookup_expr="istartswith"
-    )
-    account = filters.CharFilter(
-        field_name="account__description", lookup_expr="icontains"
-    )
-    # ordered_by = filters.ModelChoiceFilter(
-    #     field_name="ordered_by",
-    #     queryset=User.objects.all(),
-    #     label="User",
-    #     to_field_name="username",
-    # )
-    # account = filters.ModelChoiceFilter(
-    #     field_name="account",
-    #     queryset=Account.objects.all(),
-    #     label="Account",
-    #     to_field_name="description",
-    # )
-
-    class Meta:
-        model = Order
-        fields = ["name", "ordered_by", "account"]
-
-
-class OrderList(FilterView):
-    model = Order
-    filterset_class = OrderFilter
-    template_name = "inventory/order_list.html"
-    context_object_name = "order_list"
+class PaginatedFilterView(FilterView):
     paginate_by = 25
 
     def get_context_data(self, **kwargs):
@@ -58,14 +29,85 @@ class OrderList(FilterView):
         return context
 
 
+class OrderFilter(filters.FilterSet):
+    name = filters.CharFilter(field_name="name", lookup_expr="icontains", label="Name")
+    ordered_by = filters.CharFilter(
+        field_name="ordered_by__username", lookup_expr="istartswith", label="Ordered by"
+    )
+    account = filters.CharFilter(
+        field_name="account__description", lookup_expr="icontains", label="Account"
+    )
+    # example of a drop-down filter. I don't like how these look though
+    # ordered_by = filters.ModelChoiceFilter(
+    #     field_name="ordered_by",
+    #     queryset=User.objects.all(),
+    #     label="User",
+    #     to_field_name="username",
+    # )
+
+    class Meta:
+        model = Order
+        fields = ["name", "ordered_by", "account"]
+
+
+class ItemFilter(filters.FilterSet):
+    name = filters.CharFilter(
+        field_name="name", lookup_expr="icontains", label="Description"
+    )
+    vendor_or_mfg = filters.CharFilter(
+        method="by_vendor_or_mfg", label="Vendor/Manufacturer"
+    )
+    catalog_or_part = filters.CharFilter(
+        method="by_catalog_or_part", label="Catalog/Part Number"
+    )
+    category = filters.CharFilter(
+        field_name="category__name", lookup_expr="istartswith", label="Category"
+    )
+
+    def by_vendor_or_mfg(self, queryset, name, value):
+        return queryset.filter(
+            Q(vendor__name__icontains=value) | Q(manufacturer__name__icontains=value)
+        )
+
+    def by_catalog_or_part(self, queryset, name, value):
+        return queryset.filter(
+            Q(catalog__icontains=value) | Q(manufacturer_number__icontains=value)
+        )
+
+    class Meta:
+        model = Order
+        fields = [
+            "name",
+            "vendor_or_mfg",
+            "catalog_or_part",
+            "category",
+        ]
+
+
+class OrderList(PaginatedFilterView):
+    model = Order
+    filterset_class = OrderFilter
+    template_name = "inventory/order_list.html"
+    context_object_name = "order_list"
+
+
 class OrderView(generic.DetailView):
     model = Order
     template_name = "inventory/order.html"
 
     def get_context_data(self, **kwargs):
         context = super(OrderView, self).get_context_data(**kwargs)
-        context["lineitems"] = context["order"].orderitem_set.order_by("item__vendor")
+        context["orderitem_list"] = context["order"].orderitem_set.order_by(
+            "item__vendor"
+        )
         return context
+
+
+class ItemList(PaginatedFilterView):
+    model = Item
+    filterset_class = ItemFilter
+    template_name = "inventory/item_list.html"
+    context_object_name = "item_list"
 
 
 class ItemView(generic.DetailView):
