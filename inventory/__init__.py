6b8fe01325968d9dfce5bddd965f8fabd053a457
Dan Meliza <dan@meliza.org>
2025-08-11 17:11:55 -0400

    move version to pyproject.toml

--- inventory/__init__.py
+++ inventory/__init__.py
@@ -1,3 +1,9 @@
 # -*- coding: utf-8 -*-
 # -*- mode: python -*-
-__version__ = "0.6.0"
+try:
+    from importlib.metadata import version
+
+    __version__ = version("django-lab-inventory")
+except Exception:
+    # If package is not installed (e.g. during development)
+    __version__ = "unknown"
